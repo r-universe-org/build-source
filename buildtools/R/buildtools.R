8a5eab775decc94b86a3f2d1b4ef289ea39d6ad2
Jeroen Ooms <jeroenooms@gmail.com>
2021-07-22 00:12:10 +0200

    Java dependency is recursive

--- buildtools/R/buildtools.R
+++ buildtools/R/buildtools.R
@@ -153,7 +153,7 @@ install_dependencies <- function(path = '.'){
 
   # Check if rJava is a (recursive) dependency
   harddeps <- remotes::local_package_deps()
-  alldeps <- sort(unique(c(harddeps, unlist(unname(tools::package_dependencies(harddeps))))))
+  alldeps <- sort(unique(c(harddeps, unlist(unname(tools::package_dependencies(harddeps, recursive = TRUE))))))
   #cat("Hard dependencies:", paste(alldeps, collapse=', '), '\n', file = stderr())
   if(isTRUE('rJava' %in% alldeps)){
     cat('::set-output name=NEED_RJAVA::true\n')
