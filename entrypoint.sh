f87e6bd722adbc252c32c7bfb3e3a558c9143bc8
Jeroen Ooms <jeroenooms@gmail.com>
2021-09-27 21:49:14 +0200

    Default to R CMD build --no-resave-data

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,7 @@ echo "::endgroup::"
 #rm -f ${PKGDIR}/vignettes/*.Rnw
 
 # Do not build articles (vignettes) for remotes
+BUILD_ARGS="--no-resave-data"
 if [ "${5}" == "false" ]; then
   BUILD_ARGS="${BUILD_ARGS} --no-build-vignettes"
   rm -Rf ${PKGDIR}/vignettes
