a40f1899df3272df6cb367c7996e732475dbbf3c
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-27 14:19:05 +0100

    Try to fix packages depending on redland

--- buildtools/R/buildtools.R
+++ buildtools/R/buildtools.R
@@ -202,6 +202,11 @@ install_dependencies <- function(path = '.'){
     if(length(aptline) && !grepl('(libcurl|pandoc)', aptline[1])){
       system(aptline[1])
     }
+    # Special case this library due to conflicts with libcurl4-openssl-dev
+    librdf <- grep('librdf0-dev', aptline, value = TRUE)
+    if(length(librdf)){
+      system(librdf[1])
+    }
   }, error = function(e){
     message("Problem looking for system requirements: ", e$message)
   })
