c3a6a271ab59a729d4612e1540df83b97b8c7d90
Jeroen Ooms <jeroenooms@gmail.com>
2022-06-21 16:32:45 +0200

    Lower CRAN

--- buildtools/R/buildtools.R
+++ buildtools/R/buildtools.R
@@ -524,7 +524,7 @@ precache_rspm <- function(){
     unlink(list.files(tempdir(), pattern = 'packagemanager.rstudio.com', full.names = TRUE))
     pkgs <- available.packages(repos = url)
     message("Found ", nrow(pkgs), " packages on rspm")
-    if(nrow(pkgs) > 18400){
+    if(nrow(pkgs) > 18000){
       message("OK")
       break
     } else {
