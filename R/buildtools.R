0ff2eb26d6c7759914fcf7518ef0d014742dfe24
Jeroen Ooms <jeroenooms@gmail.com>
2022-09-20 16:30:38 +0200

    Migrate rundeps to contents

--- R/buildtools.R
+++ R/buildtools.R
@@ -301,7 +301,7 @@ install_dependencies <- function(path = '.'){
   harddeps <- remotes::local_package_deps()
   rundeps <- as.character(recurse_deps(harddeps))
   saveRDS(rundeps, '/tmp/rundeps.rds')
-  cat(sprintf('::set-output name=RUNDEPS::%s\n', base64_gzip(jsonlite::toJSON(rundeps))))
+  #cat(sprintf('::set-output name=RUNDEPS::%s\n', base64_gzip(jsonlite::toJSON(rundeps))))
 
   # Not used right now: mostly shows all the testthat/rmarkdown stack stuff
   #checkdeps <- setdiff(recurse_deps(setdiff(deps, rundeps)), rundeps)
