4810a0c57966f992a9a61058f8a2907833f182c5
Jeroen Ooms <jeroenooms@gmail.com>
2024-03-30 02:37:26 +0100

    add missing file

---
+++ R/issuetracker.R
@@ -0,0 +1,25 @@
+guess_development_url <- function(package, git_url){
+  desc <- as.data.frame(read.dcf(system.file('DESCRIPTION', package = package)))
+  input <- paste(desc$BugReports, desc$DevelopmentURL, desc$URL, git_url)
+  input <- normalize_github_urls(input)
+  input <- paste(input, replace_rforge_urls(input)) #Prefer GitHub URL over r-forge guess
+  input <- gsub('https://github.com/(cran|bioc)/', '', input) # No mirror URLS here
+  pattern <- 'https?://(github.com|gitlab.com|bitbucket.org)/[A-Za-z0-9_-]+/[A-Za-z0-9_.-]+'
+  m <- regexpr(pattern, input, ignore.case = TRUE)
+  urls <- regmatches(input, m)
+  sub("\\.git$", "", sub("^http://", "https://", tolower(urls)))
+}
+
+replace_rforge_urls <- function(input){
+  input <- gsub("www.rforge.net", "github.com/s-u", input, fixed = TRUE)
+  input <- gsub('r-forge\\.r-project\\.org/projects/', 'github.com/r-forge/', input, ignore.case = TRUE)
+  input <- gsub('r-forge\\.r-project\\.org/scm/[^ ]*\\?root=(\\S*)', 'github.com/r-forge/\\1', input, ignore.case = TRUE)
+  input <- gsub("https?://lists\\.r-forge\\.r-project\\.org", "", input, ignore.case = TRUE) # Remove URLS to mailing list
+  gsub("https?://([A-Za-z0-9_.-]+)\\.r-forge\\.r-project\\.org", 'https://github.com/r-forge/\\1', input, ignore.case = TRUE)
+}
+
+normalize_github_urls <- function(input){
+  input <- gsub('//www.github.com', '//github.com', input, fixed = TRUE)
+  input <- gsub("https?://([A-Za-z0-9-]+)\\.github\\.io/([A-Za-z0-9_.-]+)", 'https://github.com/\\1/\\2', input)
+  sub("https://([A-Za-z0-9-]+)\\.r-universe.dev/([A-Za-z0-9_.-]+)", 'https://github.com/\\1/\\2', input)
+}
