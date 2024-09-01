54017b7d798a74db1358faba67ff1957c24faa07
Jeroen Ooms <jeroenooms@gmail.com>
2024-09-01 11:41:41 +0200

    Add function to manually update counts

--- R/blackbird.R
+++ R/blackbird.R
@@ -25,4 +25,29 @@ make_session_handle <- function(){
   curl::new_handle(useragent = agent, cookie = sprintf("user_session=%s;", Sys.getenv("DUMMY_SESSION")))
 }
 
+update_search_results <- function(){
+  userpwd <- Sys.getenv("CRANLIKEPWD", NA)
+  if(is.na(userpwd)) stop("No CRANLIKEPWD set, cannot deploy")
+  df <- jsonlite::stream_in(url('https://r-universe.dev/stats/files?type=src&fields=_searchresults,_score'))
+  df <- df[is.na(df[['_searchresults']]),]
+  df <- df[order(df[['_score']], decreasing = TRUE),]
+  for(i in seq_len(nrow(df))){
+    info <- as.list(df[i,])
+    tryCatch({
+      count <- get_blackbird_count(info$package)
+      json <- jsonlite::toJSON(list('$set' = list('_searchresults' = count)), auto_unbox = TRUE, verbose = TRUE)
+      h <- curl::new_handle(userpwd = userpwd, copypostfields = json, httpheader = "Content-Type: application/json")
+      url <- sprintf("https://%s.r-universe.dev/packages/%s/%s/update", info$user, info$package, info$version)
+      req <- curl::curl_fetch_memory(url, handle = h)
+      if(req$status > 300){
+        stop(sprintf("Failure at %s:\n%s", url, rawToChar(req$content)))
+      }
+      message("OK: ", url)
+    }, error = function(e){
+      message(e)
+      Sys.sleep(900)
+    })
+  }
+}
+
 #out <- get_blackbird_count('dplyr')
