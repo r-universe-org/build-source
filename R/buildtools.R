4c7d00b89c3a08fc220e9baa2ff5c689417f9f81
Jeroen Ooms <jeroenooms@gmail.com>
2025-11-27 22:32:56 +0100

    Skip user_info if no universe is set

--- R/buildtools.R
+++ R/buildtools.R
@@ -591,14 +591,16 @@ universe_name_fallback <- function(){
 
 universe_info <- function(){
   name <- Sys.getenv('UNIVERSE_NAME', universe_name_fallback())
-  universe <- switch(name,
-    'bioc' = 'bioconductor',
-    'bioc-release' = 'bioconductor',
-    'r-multiverse-staging' = 'r-multiverse',
-    'ropensci-champions' = 'ropensci',
-    name
-  )
-  gh::gh(sprintf('/users/%s', universe))
+  if(nchar(name)){
+    universe <- switch(name,
+     'bioc' = 'bioconductor',
+     'bioc-release' = 'bioconductor',
+     'r-multiverse-staging' = 'r-multiverse',
+     'ropensci-champions' = 'ropensci',
+     name
+    )
+    gh::gh(sprintf('/users/%s', universe))
+  }
 }
 
 current_info <- function(package){
