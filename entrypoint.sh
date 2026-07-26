873ffdb64c1e05cd8fe44a6775aa5b826a1d928a
Jeroen Ooms <jeroenooms@gmail.com>
2026-07-26 21:54:05 +0200

    Fix rsolr

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,10 +32,10 @@ nohup Xvfb :6 -screen 0 1280x1024x24 > ~/X.log 2>&1 &
 export DISPLAY=:6
 #echo "Running fake X server on $DISPLAY"
 
-# RcppParallel is broken on winarm
-if [ "$COMMIT" = "5e55fbb09770a0daad0ca064b92408a09cebbcd1" ]; then
-URL="https://github.com/RcppCore/RcppParallel"
-COMMIT="389750a137c40a71ea1da69464f0dfe2dbab4004"
+# rsolr is broken icw bioc-devel
+if [ "$COMMIT" = "b8e4fe0abfecc333b995489c7eae30405e631fc5" ]; then
+URL="https://github.com/lawremi/rsolr"
+COMMIT="5245c109ea19640cb1b6522f53cb182ccedbcd5a"
 fi
 
 # Clone, and checkout the revision if any
